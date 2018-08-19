66c58a50a9f3514cc181f68f3b7e9b7406bef4fe
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-19 22:17:25 +0900

    add firebase database
    initialized firebase database settings and import db object to resolver context. Now db can be accessed through context. Also added json module declaration on typings.d.ts to import .json file.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { resolvers } from './resolvers';
+import db from './database/db';
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: (data) => ({
+    ...data,
+    db
+  })
 });
 
 server.start({
